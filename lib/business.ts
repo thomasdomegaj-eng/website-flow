b034d21d0c19d89312c0981cfb4d211e141b9636
thomasdomegaj-eng <thomasdomega.j@gmail.com>
2026-09-14 12:38:28 +1000

    Add Flowcoat Instagram to business config

--- lib/business.ts
+++ lib/business.ts
@@ -3,6 +3,8 @@ export const business = {
   phoneDisplay: "0447 771 304",
   phoneHref: "+61447771304",
   email: "sale@flowcoat.com.au",
+  instagramHandle: "@flow.coat",
+  instagramUrl: "https://www.instagram.com/flow.coat/",
   addressDisplay: "193–195 Power St, Glendenning NSW 2761",
   address: {
     streetAddress: "193-195 Power St",
