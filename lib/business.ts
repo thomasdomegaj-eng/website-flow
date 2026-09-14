abe31d346109809f5228464e5450092df835f716
thomasdomegaj-eng <thomasdomega.j@gmail.com>
2026-09-14 12:06:46 +1000

    Add Flowcoat sales email

--- lib/business.ts
+++ lib/business.ts
@@ -2,6 +2,7 @@ export const business = {
   name: "FLOWCOAT",
   phoneDisplay: "0447 771 304",
   phoneHref: "+61447771304",
+  email: "sale@flowcoat.com.au",
   addressDisplay: "193–195 Power St, Glendenning NSW 2761",
   address: {
     streetAddress: "193-195 Power St",
